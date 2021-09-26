481d2caa937d1868e1e573309e23e51fa14ed679
Gleb <77ripdrive@gmail.com>
2021-09-26 20:27:00 +0300

    add api login

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,73 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+let playersList;
+
+Cypress.Commands.add("setToken", (username, password, url) => {
+  Cypress.log({
+    name: "login with",
+    message: `${username} | ${password} | ${url}`,
+  });
+
+  const mutation = `
+    mutation Login ($email: String!,$pass: String!){
+        login(email: $email, pass: $pass) {
+          token
+           }
+      }`;
+
+  const variables = {
+    email: `${username}`,
+    pass: `${password}`,
+  };
+
+  cy.request({
+    method: "POST",
+    url: `${url}`,
+    headers: { "Content-Type": "application/json", Accept: "*/*" },
+    body: {
+      query: mutation,
+      variables: variables,
+    },
+    failOnStatusCode: false,
+  }).then((resp) => {
+    window.localStorage.setItem("token", resp.body.data.login.token);
+    window.localStorage.setItem("tokenStartAt", Date.now());
+
+    cy.log(resp.body.data.login.token);
+    cy.log(Date.now());
+  });
+});
+
+Cypress.Commands.add("forceVisit", (url) => {
+  cy.window().then((win) => {
+    return win.open(url, "_self");
+  });
+});
+
+let LOCAL_STORAGE_MEMORY = {};
+
+Cypress.Commands.add("saveLocalStorage", () => {
+  Object.keys(localStorage).forEach((key) => {
+    LOCAL_STORAGE_MEMORY[key] = localStorage[key];
+  });
+});
+
+Cypress.Commands.add("restoreLocalStorage", () => {
+  Object.keys(LOCAL_STORAGE_MEMORY).forEach((key) => {
+    localStorage.setItem(key, LOCAL_STORAGE_MEMORY[key]);
+  });
+});
+
+Cypress.Commands.add("waitForPageLoadAfter", (block) => {
+  // mark our window object to "know" when it gets reloaded
+  cy.window().then((win) => {
+    // eslint-disable-next-line no-param-reassign
+    win.beforeReload = true;
+  });
+  // initially the new property is there
+  cy.window().should("have.prop", "beforeReload", true);
+
+  // Run the code that triggers the page reload/change
+  block();
+
+  // after reload the property should be gone
+  cy.window().should("not.have.prop", "beforeReload");
+});
